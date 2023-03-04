48f6f1a9b644b17599a47812ed108ccee5389ff7
YevhenHryhorenko <116802765+YevhenHryhorenko@users.noreply.github.com>
2023-03-04 06:41:57 +0200

    add menu burger and fix swiper pagination

--- assets/swiper.js
+++ assets/swiper.js
@@ -1,22 +1,82 @@
-var swiper = new Swiper(".mySwiper", {
-	cssMode: true,
-	breakpoints: {
-		600: {
-			slidesPerView: 2,
-		},
-	},
-	spaceBetween: 10,
-	navigation: {
-		nextEl: ".swiper-button-next",
-		prevEl: ".swiper-button-prev",
-	},
-	pagination: {
-		el: ".swiper-pagination",
-	},
-	autoplay: {
-		delay: 2500,
-		disableOnInteraction: false,
-	},
-	mousewheel: true,
+var swiper = new Swiper(".mySwiper1", {
+    cssMode: true,
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        },
+    },
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next1",
+        prevEl: ".swiper-button-prev1",
+    },
+    pagination: {
+        el: ".swiper-pagination1",
+    },
+    mousewheel: true,
 	keyboard: true,
+	autoplay: {
+		delay: 3500,
+		disableOnInteraction: false
+	}
+});
+
+var swiper = new Swiper(".mySwiper2", {
+    cssMode: true,
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        },
+    },
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next2",
+        prevEl: ".swiper-button-prev2",
+    },
+    mousewheel: true,
+	keyboard: true,
+	autoplay: {
+		delay: 3500,
+		disableOnInteraction: false
+	}
+});
+
+var swiper = new Swiper(".mySwiper3", {
+    cssMode: true,
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        },
+    },
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next3",
+        prevEl: ".swiper-button-prev3",
+    },
+    mousewheel: true,
+	keyboard: true,
+	autoplay: {
+		delay: 3500,
+		disableOnInteraction: false
+	}
+});
+
+var swiper = new Swiper(".mySwiper4", {
+    cssMode: true,
+    breakpoints: {
+        600: {
+            slidesPerView: 2,
+        },
+    },
+    spaceBetween: 10,
+    navigation: {
+        nextEl: ".swiper-button-next4",
+        prevEl: ".swiper-button-prev4",
+    },
+    mousewheel: true,
+	keyboard: true,
+	autoplay: {
+		delay: 3500,
+		disableOnInteraction: false
+	}
 });
